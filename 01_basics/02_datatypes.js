517cbdd9e84840b08fb6a9300633d3cd70849811
Anshika Rajput <144578710+Anshika-Rajput511@users.noreply.github.com>
2024-10-27 16:52:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") //can't use here because we are using node.js not browser
+// used on console because js engine is inside browser
+
+let name = "Anshika" //string
+let age= 20; //number
+let isHonest = true; //boolean
+let fine = null;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null is a standalone value 
+//undefined is when we do not initialize a value in variable
+//symbol is used for uniqueness
+
+//object 
+
+console.log(typeof age)  //number
+console.log(typeof null)   //object
+console.log(typeof undefined)   //undefined
