bc86d198ad94d9cc5425e9d5bfd7a2663a6a8709
Anshika Rajput <144578710+Anshika-Rajput511@users.noreply.github.com>
2024-11-09 08:17:29 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+let a = 10    // local scope
+const b = 20   // local scope
+var c = 30    // global scope
+
+if(true){
+    let a=100
+    const b =200
+    console.log(a + "," + b)    // a and b inside if have their scope inside if  // they have block scope
+}
+console.log(a);  // 10
+console.log(b);  // 20
+console.log(c);  // 30
+
+for(let i=1; i<4; i++){
+    console.log(i);    // i have bock scope
+}
