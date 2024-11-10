3b26f3e2c3f14def9f513e4abd2ae1339fc317f0
Anshika Rajput <144578710+Anshika-Rajput511@users.noreply.github.com>
2024-11-10 12:44:37 +0000

    for loop

---
+++ 05_iterations.js/01_loops.js
@@ -0,0 +1,21 @@
+// for 
+
+for(let i=0; i<5;i++)
+{
+    let element = i;
+    console.log(element);
+}
+
+for(let i=0;i<5;i++)
+{
+    console.log(`outer loop, ${i}`) 
+    for(let j=0;j<2;j++)
+    {
+       console.log(`inner loop, ${j}`) 
+    }
+}
+
+
+//break - exit out of loop
+// continue - skip the iteration
+
