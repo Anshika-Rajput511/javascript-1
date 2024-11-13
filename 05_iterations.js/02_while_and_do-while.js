0fe7d5230a32483f372451ad394f9423f1821354
Anshika Rajput <144578710+Anshika-Rajput511@users.noreply.github.com>
2024-11-13 07:24:58 +0000

    while and do-while

---
+++ 05_iterations.js/02_while_and_do-while.js
@@ -0,0 +1,21 @@
+let index=0
+while(index<=2)
+{
+    console.log(`value of index: ${index}`);
+    index++;
+}
+
+let myArray = [11,12,13,14,15]
+let arr=0;
+while(arr<myArray.length)
+{
+    console.log(`value is : ${myArray[arr]}`);
+    arr++;
+} 
+
+let score=10;
+do{
+    console.log(`score is : ${score}`);
+    score++;
+}while(score<5)    // loop execute at least once
+
